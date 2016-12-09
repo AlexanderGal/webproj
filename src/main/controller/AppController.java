2ba3161d8c985537fcf5c6de2fdb9cb65c198d0f
AlexanderGal <balron923@gmail.com>
2016-12-09 15:17:53 +0300

    test data add

--- src/main/controller/AppController.java
+++ src/main/controller/AppController.java
@@ -1,14 +1,27 @@
 package main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap; 
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import main.entity.Bank;
+import main.entity.Client;
+import main.repository.BankRepository;
+import main.repository.ClientRepository;
+
 @Controller
 public class AppController {
+	@Autowired
+	private BankRepository bankRepository;
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String printWelcome(ModelMap model){
 		model.addAttribute("title","Spring MVC + MVN + @JavaConfig");
@@ -20,6 +33,26 @@ public class AppController {
 		ModelAndView model = new ModelAndView();
 		model.setViewName("index");
 		model.addObject("msg", name);
+		return model;
+	}
+	
+	@RequestMapping(value="/addbank", method = RequestMethod.GET)
+	public ModelAndView addBank(@ModelAttribute("Bank")Bank bank, BindingResult result){
+			
+		//test block
+		Bank bank1 = new Bank("Bank");
+		bankRepository.save(bank1);
+		Client client = new Client();
+		client.setAddress("Adress");
+		client.setBank(bank1);
+		client.setEmail("test@email.com");
+		client.setFirstName("first name");
+		client.setLastName("last name");
+		clientRepository.save(client);
+		//
+		
+		ModelAndView model = new ModelAndView();
+		model.setViewName("index");
 		return model;
 	}
 }
