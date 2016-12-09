bde13d6356faefdf09755ca2f0547083c9dac5d7
AlexanderGal <balron923@gmail.com>
2016-12-09 11:02:07 +0300

    pom fix

--- src/main/config/PersistanceConfig.java
+++ src/main/config/PersistanceConfig.java
@@ -1,12 +1,10 @@
 package main.config;
 
-import java.util.Properties;
 
 import javax.annotation.Resource;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -31,10 +29,7 @@ public class PersistanceConfig {
 	private static final String PROP_DATABASE_PASSWORD = "db.password";
 	private static final String PROP_DATABASE_URL = "db.url";
 	private static final String PROP_DATABASE_USERNAME = "db.username";
-	private static final String PROP_HIBERNATE_DIALECT = "db.hibernate.dialect";
-//	private static final String PROP_HIBERNATE_SHOW_SQL = "db.hibernate.show.sql";
 	private static final String PROP_ENTITYMANAGER_PACKAGETS_TO_SCAN = "db.entitymanager.packages.to.scan";
-//	private static final String PROP_HIBERNATE_HBM2DDL_AUTO = "db.hibernate.hdbm2ddl.auto";
 	
 	@Resource
 	private Environment env;
@@ -53,10 +48,8 @@ public class PersistanceConfig {
 	public LocalContainerEntityManagerFactoryBean entityManagerFactor(){
 		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
 		entityManagerFactory.setDataSource(dataSource());
-//		entityManagerFactory.setPersistenceProviderClass(HibernatePersistenceProvider.class);
 		entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter());
 		entityManagerFactory.setPackagesToScan(env.getRequiredProperty(PROP_ENTITYMANAGER_PACKAGETS_TO_SCAN));
-//		entityManagerFactory.setJpaProperties(getHibernateProperties());
 		return entityManagerFactory;
 	}
 
@@ -64,14 +57,6 @@ public class PersistanceConfig {
 	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
 		return new JpaTransactionManager(emf);
 	}
-	
-//	private Properties getHibernateProperties() {
-//		Properties prop = new Properties();
-//		prop.put(PROP_HIBERNATE_DIALECT, env.getRequiredProperty(PROP_HIBERNATE_DIALECT));
-//		prop.put(PROP_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROP_HIBERNATE_SHOW_SQL));
-//		prop.put(PROP_HIBERNATE_HBM2DDL_AUTO, env.getRequiredProperty(PROP_HIBERNATE_HBM2DDL_AUTO));
-//		return prop;
-//	}
 	
 	 @Bean
 	    public JpaVendorAdapter jpaVendorAdapter() {
